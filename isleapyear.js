5c464f1790bb35baa29479f3c0d699b07b1a494e
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-07-04 13:49:05 +0100

    leapyear tester

--- isleapyear.js
+++ isleapyear.js
@@ -1,10 +1,14 @@
 // This function takes in a year and tells you if it is a leap year or not
 
 function isLeapYear(year) {
-    if (year % 4 === 0 ) {
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0 ) {
         console.log("It's a leap year baby!");
-    } elif(year % 4 === 0 && year % 100 === 0 && year % 400 === 0) 
+    } else if(year % 4 === 0 ) {
+        console.log("It's a leap year baby!")
+    } else{
+        console.log("It's not a leap year")
+    }
     
 }
 
-isLeapYear(2100)
+isLeapYear(2000)
