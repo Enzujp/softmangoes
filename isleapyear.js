e0904410c2cda02b9e27d6d3347c9b5611a008b4
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-07-04 13:36:33 +0100

    mixing conditionals

---
+++ isleapyear.js
@@ -0,0 +1,10 @@
+// This function takes in a year and tells you if it is a leap year or not
+
+function isLeapYear(year) {
+    if (year % 4 === 0 ) {
+        console.log("It's a leap year baby!");
+    } elif(year % 4 === 0 && year % 100 === 0 && year % 400 === 0) 
+    
+}
+
+isLeapYear(2100)
---
+++ random.js
@@ -0,0 +1,17 @@
+// Building a love calculator with javascript.
+
+var loveScore = Math.random() * 100
+var trueLove = Math.floor(loveScore) + 1
+
+
+
+if (trueLove > 60) {
+    console.log("This really must be what true love feels and looks like")
+} else {
+    console.log("Nah Man, you both know that this is not going to work out")
+}
+
+
+
+
+console.log(trueLove)
