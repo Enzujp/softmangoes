d6233f2c3a3d07f06b3c6dbf39e186cde11aa227
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-08-08 15:17:19 +0100

    using a constructor function"

--- dom/new.js
+++ dom/new.js
@@ -11,3 +11,32 @@ var bellBoy1 = {
 }
 
 //And use the dot '.' property to access the inner properties for bellBoy1
+
+var houseKeeper1 = {
+    name: 'jenny',
+    yearsOfExperience: 6,
+    married: false,
+    age: 21
+}
+
+
+console.log(`this is ${houseKeeper1.name} and she's going to be working for us`);
+
+// Too avoid repetition of code time and time again, create a function that takes in all the necessary
+// information required for the class or group
+
+
+//This is an example of a Constructor function that takes in 4 inputs
+// and using this. notation matches the inputs to the property names.
+
+
+function BellBoy(name, age, hasWorkPermit, languages) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
+}
+
+// to create an object from this, we would write the code thus:
+
+var bellBoy2 = new BellBoy('Sammy', 19, true, ['english', 'spanish']); //This is how you initialize the object
