92787b0e950cdd1d874582249900afb8ed71fc4c
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-08-08 15:05:57 +0100

    added objects to var

--- dom/new.js
+++ dom/new.js
@@ -1,2 +1,13 @@
 // This is a test of my typing skills and see just how many words I can get in in a minute or so
-// This app shows how to work with objects
+// This app shows how to work with 
+// Objects are in javascript what Dictionaries are in python 
+
+var bellBoy1 = {
+    name: 'Timmy',
+    age: '20',
+    languages: [
+        'spanish', 'dutch'
+    ]
+}
+
+//And use the dot '.' property to access the inner properties for bellBoy1
